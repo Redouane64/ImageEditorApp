f3b9fce4635f2a061b6e96f77a5dcc997f231450
Redhouane Sobaihi <s-redouane@outlook.com>
2019-05-02 14:56:56 +0700

    Invert image colors.

---
+++ src/ImageEditor/Services/ImageEditingService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Services
+{
+    public class ImageEditingService
+    {
+        public ImageEditingService() { }
+
+        public SkiaSharp.SKColor[] InvertColors(SkiaSharp.SKColor[] pixels)
+        {
+            SkiaSharp.SKColor[] newPixels = new SkiaSharp.SKColor[pixels.Length];
+
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                Byte r = (byte)(255 - pixels[i].Red);
+                Byte g = (byte)(255 - pixels[i].Green);
+                Byte b = (byte)(255 - pixels[i].Blue);
+
+                newPixels[i] = new SkiaSharp.SKColor(r, g, b);
+            }
+
+            return newPixels;
+        }
+    }
+}
